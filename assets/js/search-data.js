cd38b9796ce883ffdac33a622138b19f64beeea8
Milenaw <milena.weiershausen@gmx.de>
2025-10-26 14:14:50 +0000

    Deploying to gh-pages from @ Milenaw/milenaw.github.io@aa750d4d33246418ac707214a9263e8847e10db8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,22 +9,22 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-activities",
+  },{id: "nav-activities",
           title: "Activities",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/activities/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-teaching",
-          title: "Teaching",
+          title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
